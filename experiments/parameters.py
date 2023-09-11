9225591934ea90e8ce2e74bc3b5712f4406d05a9
BenjaminCarrel <benjamin.carrel@outlook.com>
2023-09-11 16:00:13 +0200

    parameters for figures

---
+++ experiments/parameters.py
@@ -0,0 +1,30 @@
+
+#%% MATPLOTLIB PARAMETERS
+import matplotlib.pyplot as plt
+
+# Save original parameters
+original_params = plt.rcParams.copy()
+
+# Custom parameters
+plt.rcParams['font.size'] = 14
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.rcParams['figure.dpi'] = 100
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['lines.markersize'] = 10
+# plt.rcParams['axes.grid'] = True
+plt.rcParams['axes.linewidth'] = 2
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['axes.titlesize'] = 14
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['legend.frameon'] = False
+plt.rcParams['legend.loc'] = 'best'
+plt.rcParams['figure.autolayout'] = True
+
+# Save custom parameters
+custom_params = plt.rcParams.copy()
+
+# Parameters for saving the plots
+path = 'figures/'
+do_save = True
