0b51666af7cc54beeb45cc9b10512310e3559f90
BenjaminCarrel <benjamin.carrel@outlook.com>
2023-11-21 21:51:43 +0100

    Add Allen-Cahn experiments

---
+++ experiments/Allen-Cahn/plot_over_time.py
@@ -0,0 +1,150 @@
+"""
+File for plotting the solution over over time.
+
+Author: Benjamin Carrel, University of Geneva, 2023
+"""
+
+#%% IMPORTATIONS
+from graphics_parameters import *
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from IPython.display import HTML
+from datetime import datetime
+from problems import make_allen_cahn
+from low_rank_toolbox import SVD, LowRankMatrix
+from matrix_ode_toolbox.integrate import solve_matrix_ivp
+import numpy as np
+from matrix_ode_toolbox.dlra import solve_dlra
+
+
+#%% SETUP THE ODE
+size = 256
+t_span = (0, 10)
+make_ode = make_allen_cahn
+ode, X0 = make_ode(size)
+nb_steps = 100
+
+
+# Print the ode defined in setup_problem.py
+print(ode)
+if not isinstance(X0, LowRankMatrix):
+    X0 = SVD.reduced_svd(X0)
+print('Dimensions: ', X0.shape)
+
+#%% Reference solution
+ts = np.linspace(*t_span, nb_steps+1)
+ref_sol = solve_matrix_ivp(ode, t_span, X0, solver="scipy", t_eval=ts, monitor=True, scipy_method='RK45')
+Xs_ref = ref_sol.todense()
+# print('Time taken by the reference solver (scipy LSODA with tol=1e-8): {:.2f} s'.format(ref_sol.computation_time))
+
+#%% Plot at different times
+time_to_plot = [0, 3, 5, 7, 10]
+nb_plots = len(time_to_plot)
+fig, axs = plt.subplots(1, nb_plots, figsize=(22, 4), sharex=True, sharey=True)
+# fig.suptitle(f'Reference solution')
+
+for i, t in enumerate(time_to_plot):
+    frame_number = np.argmin(np.abs(ref_sol.ts - t))
+    rotated_img = ndimage.rotate(Xs_ref[frame_number], 0)
+    im = axs[i].imshow(rotated_img, cmap='jet', extent=[0, 2*np.pi, 0, 2*np.pi], interpolation='bilinear')
+    axs[i].set_title('t = {:.2f}'.format(t), fontsize=24)
+    axs[i].set_xticks([0, np.pi, 2*np.pi], [0, r'$\pi$', r'$2\pi$'])
+    axs[i].set_yticks([0, np.pi, 2*np.pi], [0, r'$\pi$', r'$2\pi$'])
+    # axs[i].set_xlabel(r'$x$')
+    # axs[i].set_ylabel(r'$y$')
+    axs[i].grid(False)
+
+# Move the color bar outside of the plots
+divider = make_axes_locatable(axs[-1])
+cax = divider.append_axes("right", size="5%", pad=0.5)
+fig.colorbar(im, cax=cax, orientation='vertical')
+
+plt.tight_layout()
+
+# Save it with timestamp
+timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+fig.savefig(f'figures/{ode_name}_reference_solution_over_time_{timestamp}.pdf', bbox_inches='tight')
+
+
+plt.show()
+
+# %% DLRA solution
+
+# Initial value
+rank = 10
+Y0 = SVD.truncated_svd(X0, rank)
+
+# Solver parameters
+k = 2
+poles = np.repeat(k / np.sqrt(2), k)
+krylov_kwargs = {'size': k, 'kind': 'rational', 'poles': poles}
+order = 1
+kwargs = {'order': order, 'krylov_kwargs': krylov_kwargs, 'use_closed_form': False}
+
+# Solve
+dlra_sol = solve_dlra(ode, t_span, Y0, dlra_solver="PERK", solver_kwargs=kwargs, t_eval=ts, monitor=True)
+Ys = dlra_sol.todense()
+
+#%% Plot at different times
+fig, axs = plt.subplots(1, nb_plots, figsize=(22, 4), sharex=True, sharey=True)
+# fig.suptitle(fr'Proj. exp. Euler ($r={rank}$)')
+
+for i, t in enumerate(time_to_plot):
+    frame_number = np.argmin(np.abs(dlra_sol.ts - t))
+    rotated_img = ndimage.rotate(Ys[frame_number], 0)
+    im = axs[i].imshow(rotated_img, cmap='jet', extent=[0, 2*np.pi, 0, 2*np.pi], interpolation='bilinear')
+    axs[i].set_title('t = {:.2f}'.format(t), fontsize=24)
+    axs[i].set_xticks([0, np.pi, 2*np.pi], [0, r'$\pi$', r'$2\pi$'])
+    axs[i].set_yticks([0, np.pi, 2*np.pi], [0, r'$\pi$', r'$2\pi$'])
+    # axs[i].set_xlabel(r'$x$')
+    # axs[i].set_ylabel(r'$y$')
+    axs[i].grid(False)
+
+# Move the color bar outside of the plots
+divider = make_axes_locatable(axs[-1])
+cax = divider.append_axes("right", size="5%", pad=0.5)
+fig.colorbar(im, cax=cax, orientation='vertical')
+
+plt.tight_layout()
+
+# Save it with timestamp
+timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+fig.savefig(f'figures/{ode_name}_perk{order}_solution_over_time_{timestamp}.pdf', bbox_inches='tight')
+
+plt.show()
+
+
+#%% Plot the error at different times
+fig, axs = plt.subplots(1, nb_plots, figsize=(22, 4), sharex=True, sharey=True)
+# fig.suptitle(f'Predictive error')
+
+for i, t in enumerate(time_to_plot):
+    frame_number = np.argmin(np.abs(dlra_sol.ts - t))
+    error = (Ys[frame_number] - Xs_ref[frame_number]) / np.linalg.norm(Xs_ref[frame_number], 'fro')
+    rotated_img = ndimage.rotate(error, 0)
+    im = axs[i].imshow(rotated_img, cmap='jet', extent=[0, 2*np.pi, 0, 2*np.pi], interpolation='bilinear')
+    axs[i].set_title('t = {:.2f}'.format(t), fontsize=24)
+    axs[i].set_xticks([0, np.pi, 2*np.pi], [0, r'$\pi$', r'$2\pi$'])
+    axs[i].set_yticks([0, np.pi, 2*np.pi], [0, r'$\pi$', r'$2\pi$'])
+    # axs[i].set_xlabel(r'$x$')
+    # axs[i].set_ylabel(r'$y$')
+    axs[i].grid(False)
+
+# Move the color bar outside of the plots
+divider = make_axes_locatable(axs[-1])
+cax = divider.append_axes("right", size="5%", pad=0.5)
+cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+
+# plt.tight_layout()
+
+# Save the figure with timestamp
+timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+fig.savefig(f'figures/{X0.shape}_error_over_time_{timestamp}.pdf', bbox_inches='tight')
+
+# Display the plot
+plt.show()
+
+
+
+# %%
---
+++ experiments/Allen-Cahn/problems.py
@@ -0,0 +1,73 @@
+"""
+File for generating the Allen-Cahn problem.
+
+Author: Benjamin Carrel, University of Geneva
+"""
+
+# %% IMPORTATIONS
+import numpy as np
+from low_rank_toolbox import SVD, LowRankMatrix
+from matrix_ode_toolbox import SylvesterLikeOde
+from scipy import sparse
+
+
+def make_allen_cahn(size: int):
+    """
+    Allen-Cahn equation
+        X' = AX + XA + X - X^3
+        X(0) = X0
+    where A is the 1D Laplacian (times epsilon) as stencil 1/dx^2 [1 -2 1] in csc format, periodic BC
+    """
+    ## DISCRETIZATION
+    xs = np.linspace(0, 2 * np.pi, size)
+    dx = xs[1] - xs[0]
+    X, Y = np.meshgrid(xs, xs)
+
+    ## OPERATOR: A is the 1D Laplacian as stencil 1/dx^2 [1 -2 1] in csc format, periodic BC
+    epsilon = 0.01
+    # A = epsilon * laplacian_1d_dx2(size, dx, periodic=True)
+    A = epsilon * laplacian_1d_dx4(size, dx, periodic=True)
+
+    ## SOURCE: G(t) = X - X^3
+    def G(t, X):
+        if isinstance(X, LowRankMatrix):
+            return X - X.hadamard(X).hadamard(X)
+        else:
+            return X - X**3
+    
+    ## DEFINE THE ODE
+    ode = SylvesterLikeOde(A, A, G)
+
+    ## INITIAL VALUE
+    u = lambda x, y: (np.exp(-np.tan(x)**2) + np.exp(-np.tan(x)**2)) * np.sin(x) * np.sin(y) / (1 + np.exp(np.abs(1/np.sin(-x/2))) + np.exp(np.abs(1/np.sin(-y/2))))
+    f = lambda x, y: u(x,y) # - u(x, 2*y) + u(3*x + np.pi, 3*y + np.pi) - 2*u(4*x, 4*y) + 2 * u(5*x, 5*y)
+    X0 = f(X, Y)
+    X0 = SVD.truncated_svd(X0)
+
+    return ode, X0
+
+## Laplacian with error O(dx^2)
+def laplacian_1d_dx2(n, dx, periodic=False):
+    """
+    Discrete Laplacian matrix in 1D (error O(dx^2))
+    """
+    DD = sparse.diags([1, -2, 1], [-1, 0, 1], shape=(n, n), format='csc') / (dx ** 2)
+    if periodic:
+        DD[0, -1] = 1 / (dx ** 2)
+        DD[-1, 0] = 1 / (dx ** 2)
+    return DD
+
+## Laplacian with error O(dx^4)
+def laplacian_1d_dx4(n, dx, periodic=False):
+    """
+    Discrete Laplacian matrix in 1D (error O(dx^4))
+    """
+    DD = sparse.diags([-1, 16, -30, 16, -1], [-2, -1, 0, 1, 2], shape=(n, n), format='csc') / (12 * dx ** 2)
+    if periodic:
+        DD[0, -2] = -1 / (12 * dx ** 2)
+        DD[0, -1] = 16 / (12 * dx ** 2)
+        DD[1, -1] = -1 / (12 * dx ** 2)
+        DD[-1, 0] = 16 / (12 * dx ** 2)
+        DD[-1, 1] = -1 / (12 * dx ** 2)
+        DD[-2, 0] = -1 / (12 * dx ** 2)
+    return DD
